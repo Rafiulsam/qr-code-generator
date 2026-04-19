697a2b9fa7cde6aa1b91d11b19ec4f603139ca44
Rafiul Sam <rafiulsam69@gmail.com>
2026-04-20 02:26:38 +0600

    some changes

--- js/app.js
+++ js/app.js
@@ -8,12 +8,20 @@ generateBtn.addEventListener('click', () => {
     const inputValue = inputFiled.value
     inputFiled.value = '';
     const qrLink = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`
-    qrImage.src = qrLink
+
+    if (inputValue === '') {
+        alert('Please enter a value to generate QR code')
+    }
+    else {
+        qrImage.src = qrLink
+        downloadBtn.style.display = "block"
+    }
+
 })
 
-downloadBtn.addEventListener('click', async () =>{
+downloadBtn.addEventListener('click', async () => {
     const res = await fetch(qrImage.src)
-    const data= await res.blob()
+    const data = await res.blob()
     const downloadLink = document.createElement('a')
     downloadLink.href = URL.createObjectURL(data)
     downloadLink.download = "qrcode.jpg"
